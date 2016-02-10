09d66b5b80e068f1bd577e242f6f7d77d9ad0c16
katsuraku <kirsten.jones@ft.com>
2016-02-10 09:08:42 +0000

    Handler can specify an error code
    plus comment

--- src/clojure_webapp/core.clj
+++ src/clojure_webapp/core.clj
@@ -1,9 +1,12 @@
 (ns clojure-webapp.core)
 
 ;; now any http request will return the contents of the file test.txt
+;; can specify a status code. 500 will return that error code instead of a 200. response body stays the same.
+;; try curl -v http://localhost:3000 to see it
 
 (defn example-handler [request]
- {:body (java.io.File. "test.txt")}
+ {:body (java.io.File. "test.txt")
+  :status 500}
  )
 
 (defn on-init []
